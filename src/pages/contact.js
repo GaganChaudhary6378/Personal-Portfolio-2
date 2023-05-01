518147284b69ec99af5ee72eaa7ba647703efa82
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-05-01 23:08:13 +0530

    added responsiveness for mobiles for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,20 @@
 export default function about() {
   return (
-    <div className="container my-14 px-6 ">
+    <div className="container md:my-14 md:px-6 my-24 px-[160px]">
       <section className="mb-32 text-gray-800 text-center">
         <div className="px-6 py-12 md:px-12">
           <div className="container mx-auto xl:px-32">
             <div className="grid lg:grid-cols-2 items-center">
               <div className="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
                 <div className="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14">
-                  <h2 className="text-3xl font-bold mb-12 font-revalia text-[#245D27]">Contact Me</h2>
-                  <form>
+                  <h2 className="md:text-3xl font-bold mb-12 font-revalia text-[#245D27] text-4xl">
+                    Contact Me
+                  </h2>
+                  <form className="md:w-auto w-[500px] h-[400px] md:h-auto mb-0">
                     <div className="form-group mb-6 font-revalia">
                       <input
                         type="text"
-                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
+                        className="form-control block md:w-full w-[600px] h-[70px] md:px-3 px-5 md:h-auto py-1.5 md:text-base text-[1.5rem] font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
                         id="exampleInput7"
                         placeholder="Name"
                       />
@@ -20,14 +22,14 @@ export default function about() {
                     <div className="form-group mb-6 font-revalia">
                       <input
                         type="email"
-                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
+                        className="form-control block md:w-full w-[600px] h-[70px] md:px-3 px-5 py-1.5 md:h-auto md:text-base text-[1.5rem] font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
                         id="exampleInput8"
                         placeholder="Email address"
                       />
                     </div>
                     <div className="form-group mb-6 font-revalia">
                       <textarea
-                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
+                        className="form-control block md:w-full w-[600px] md:px-3 px-5 md:py-1.5 py-5 md:h-auto h-[70px] md:text-base text-[1.5rem] font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#245D27] focus:outline-none"
                         id="exampleFormControlTextarea13"
                         rows="3"
                         placeholder="Message"
@@ -36,7 +38,7 @@ export default function about() {
 
                     <button
                       type="submit"
-                      className="w-full px-6 py-2.5 bg-[#B0F2B2] font-revalia font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-[#245D27] hover:text-white hover:shadow-lg focus:bg-[#245D27] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#245D27] active:shadow-lg transition duration-150 ease-in-out"
+                      className="md:w-full w-[600px] px-6 py-2.5 md:h-auto h-[40px] bg-[#B0F2B2] font-revalia font-bold md:text-xs text-[1rem] leading-tight uppercase rounded shadow-md hover:bg-[#245D27] hover:text-white hover:shadow-lg focus:bg-[#245D27] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#245D27] active:shadow-lg transition duration-150 ease-in-out"
                     >
                       Send
                     </button>
