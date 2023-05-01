4f20cc8c285c9ccc67d5fb2f9560c5cc2936d52c
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-05-01 19:52:41 +0530

    added typewriter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
+import Typewriter from "typewriter-effect";
 export default function landing() {
   return (
     <div className="mt-[160px]">
@@ -9,9 +9,17 @@ export default function landing() {
       </h3>
       <div className="mt-5 font-poppins font-bold text-[80px] leading-[100px] mx-12">
         <p className="text-[#245D27]">Hi! I’m Gagan</p>
-        <p className="text-[#245D27]">
-          Full Stack <span className="text-[#54AE58]">Developer</span>
-        </p>
+        {/* <p className="text-[#245D27]"> */}
+        <div id="app" className="text-[#54AE58]">
+          <Typewriter
+            options={{
+              strings: ["Full Stack Developer."],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        {/* </p> */}
       </div>
       <div>
         <p className="font-revalia w-[800px] pt-6 mx-12">
@@ -21,8 +29,14 @@ export default function landing() {
         </p>
       </div>
       <div className="btn">
-        <button className=" mx-12 bg-[#B0F2B2] w-[123px] h-[42px] font-revalia text-[17px] rounded-md">Resume!</button>
-        <Link href="/projects"><button className="glow-on-hover ml-4 font-revalia font-normal text-[17px] bg-[#F5F5F5] w-[217px] h-[42px]">See my Projects</button></Link>
+        <button className=" mx-12 bg-[#B0F2B2] w-[123px] h-[42px] font-revalia text-[17px] rounded-md">
+          Resume!
+        </button>
+        <Link href="/projects">
+          <button className="glow-on-hover ml-4 font-revalia font-normal text-[17px] bg-[#F5F5F5] w-[217px] h-[42px]">
+            See my Projects
+          </button>
+        </Link>
       </div>
     </div>
   );
