08bcb996246d7c77e3a678e8caf2c5d86dcc4707
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-05-01 13:07:07 +0530

    added buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
           <p className="w-80 text-3xl font-bold line-height-[39px] leading-6 font-poppins text-[#245D27]">Gagan Chaudhary</p>
         </Link>
       </div>
-      <div className="mx-[28%] nav w-full">
+      <div className="mx-[36%] nav w-full">
         <ul className="flex mt-0 font-bold font-poppins">
           <Link href={"/about"}>
             <li className="w-40 text-xl font-bold font-poppins text-[#245D27]">About Me</li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import Image from "next/image";
 export default function landing() {
   return (
-    <div className="mt-[180px] mx-12 font-bold font-poppins">
-      <h3 >Welcome to my personal portfolio.</h3>
-      <div className="mt-5 font-poppins font-bold text-[80px] leading-[100px]">
+    <div className="mt-[180px]">
+      <h3 className="font-bold font-poppins mx-12">
+        Welcome to my personal portfolio.
+      </h3>
+      <div className="mt-5 font-poppins font-bold text-[80px] leading-[100px] mx-12">
         <p className="text-[#245D27]">Hi! I’m Gagan</p>
-        <p className="text-[#245D27]">Full Stack <span className="text-[#54AE58]">Developer</span></p>
+        <p className="text-[#245D27]">
+          Full Stack <span className="text-[#54AE58]">Developer</span>
+        </p>
+      </div>
+      <div>
+        <p className="font-revalia w-[800px] pt-6 mx-12">
+          Lorem ipsum dolor sit amet consectetur. Arcu est vitae ullamcorper in
+          dolor ut turpis vitae iaculis. Egestas tempor dictum nam lorem
+          vulputate etiam iaculis volutpat.
+        </p>
+      </div>
+      <div className="btn">
+        <button className="pt-[60px] mx-12">Hire Me!</button>
       </div>
     </div>
   );
