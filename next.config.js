6dec159e87b87ddb01591b02f16b6479f7caa3f5
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-05-05 18:07:22 +0530

    nodemailer working and added body to it

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
 }
 
 module.exports = {
-
-
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
--- src/pages/api/postdetails.js
+++ src/pages/api/postdetails.js
@@ -1,16 +1,38 @@
 import connectDb from "../../../middleware/mongoose";
 import contact from "../../../models/contact";
+import nodemailer from "nodemailer";
 
-const handler =async(req,res) => {
-    if(req.method=='POST'){
-        const {name,email,message}=req.body;
-        let u=new contact({name,email,message})
-
-        await u.save();
-        res.status(200).json({success:"success"})
-    }else{
-        res.status(400).json({error:"This method is not allowed"});
+const handler = async (req, res) => {
+    const { name, email, message } = req.body;
+    if (req.method == "POST") {
+      let u = new contact({ name, email, message });
+      // Nodemailer
+      await u.save();
     }
-}
+  
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "gss12111408@gmail.com",
+      pass: "mepqqsppvvxfwden",
+    },
+    secure: true,
+  });
+  const mailData = {
+    from: "gss12111408@gmail.com",
+    to: email,
+    subject: `Message From Gagan Chaudhary Portfolio Site`,
+    text: "Sent from: " + "gss12111408@gmail.com",
+    html: `<div>Thanks Mr./Mrs. ${name} for contacting Mr. Gagan Chaudhary ,will respond to your query i.e ( ${message} ) in 1-2 business days.</div><h3>Have a good day.</h3><p>Sent from: gss12111408@gmail.com</p>`,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) 
+    console.log("an error occured");
+    else console.log("done");
+  });
+  res.status(200).json({success:"success"})
+};
 
 export default connectDb(handler);
