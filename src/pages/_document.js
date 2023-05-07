dae7508ee6d7caeffba37b87c310f9698fc7733f
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-05-07 22:44:21 +0530

    almost completed skills page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link href="https://languages.abranhe.com/logos.css" rel="stylesheet"></link>
       <Head />
       <body>
         <Main />
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -7,13 +7,17 @@ import { FaReact } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiMongodb } from "react-icons/si";
 import { IoLogoNodejs} from "react-icons/io";
-import { TbBrandCpp} from "react-icons/tb";
+import { BsGit} from "react-icons/bs";
+import { AiFillGithub} from "react-icons/ai";
+import { IoLogoVercel} from "react-icons/io5";
+import { SiTypescript} from "react-icons/si";
 
+import Image from "next/image";
 
 
 export default function about() {
   return (
-    <>
+    <div>
       {/* Web development */}
       <div className="mt-6 ml-12">
         <p className=" text-xl text-[#3fa244] font-semibold font-revalia">
@@ -24,9 +28,9 @@ export default function about() {
         <p className="mt-9 ml-4 font-revalia">HTML</p>
         <SiCss3 className="mt-6 text-6xl ml-[130px] text-blue-700"/>
         <p className="mt-9 ml-4 font-revalia ">CSS</p>
-        <SiJavascript className="mt-6 text-6xl ml-[130px] text-yellow-300"/>
-        <p className="mt-9 ml-4 font-revalia ">JavaScript</p>
-        <SiTailwindcss className="mt-6 text-6xl ml-[120px] text-sky-400"/>
+        <SiJavascript className="mt-6 text-6xl ml-[135px] text-yellow-300"/>
+        <p className="mt-9 ml-6 font-revalia ">JavaScript</p>
+        <SiTailwindcss className="mt-6 text-6xl ml-[110px] text-sky-400"/>
         <p className="mt-9 ml-4 font-revalia ">Tailwind CSS</p>
         <FaReact className="mt-6 text-6xl ml-[120px] text-sky-400"/>
         <p className="mt-9 ml-4 font-revalia ">ReactJS</p> 
@@ -36,33 +40,54 @@ export default function about() {
         <p className="mt-[70px] ml-4 font-revalia ">NextJS</p> 
         <SiMongodb className="mt-14 text-6xl ml-[110px] text-green-600"/>
         <p className="mt-[70px] ml-4 font-revalia ">MongoDB</p> 
-        <IoLogoNodejs className="mt-14 text-6xl ml-[80px] text-green-600"/>
-        <p className="mt-[70px] ml-4 font-revalia ">NodeJS</p> 
+        <IoLogoNodejs className="mt-14 text-6xl ml-[85px] text-green-600"/>
+        <p className="mt-[70px] ml-6 font-revalia ">NodeJS</p> 
         </div>
         
-      </div>
+      </div>    
       {/* Data structures */}
       <div className="mt-[70px] ml-12">
         <p className=" text-xl text-[#3fa244] font-semibold font-revalia">
           Data Structures
         </p>
-        <div className="flex flex-row">
-        <TbBrandCpp className="mt-6 text-6xl "/>
-        <p className="mt-10 ml-4 font-revalia">Cpp</p>
+        <div className="flex flex-row mt-8">
+        <i className="programming lang-cpp mt-6 text-5xl"></i>
+        <p className="mt-12 ml-4 font-revalia">Cpp</p>
+        <i className="programming lang-c mt-8 ml-[125px] text-5xl"></i>
+        <p className="mt-12 ml-6 font-revalia">C</p>
+        <i className="programming lang-java mt-5 ml-[150px] text-5xl"></i>
+        <p className="mt-12 ml-6 font-revalia">Java</p>
+        
         </div>
       </div>
       {/* Others */}
-      <div className="mt-[200px] ml-12">
+      <div className="mt-[70px] ml-12">
         <p className=" text-xl text-[#3fa244] font-semibold font-revalia">
           Others
         </p>
+        <div className="flex flex-row mt-6">
+        <BsGit className="mt-8 text-6xl text-red-600"/>
+        <p className="mt-[50px] ml-4 font-revalia ">Git</p> 
+        <AiFillGithub className="mt-8 text-6xl ml-[155px]"/>
+        <p className="mt-[50px] ml-8 font-revalia ">Github</p> 
+        <IoLogoVercel className="mt-8 text-6xl ml-[120px]"/>
+        <p className="mt-[50px] ml-6 font-revalia ">Vercel</p> 
+        <Image src="figma-icon.svg" width={50} height={50} className="mt-8 ml-[155px]"></Image>
+        <p className="mt-[50px] ml-6 font-revalia ">Figma</p> 
+        </div>
       </div>
       {/* Learning*/}
-      <div className="mt-[200px] ml-12">
+      <div className="mt-[70px] ml-12">
         <p className=" text-xl text-[#3fa244] font-semibold font-revalia">
           Learning
         </p>
+        <div className="flex flex-row mt-6">
+        <SiTypescript className="mt-8 text-6xl text-blue-500"/>
+        <p className="mt-[50px] ml-4 font-revalia ">TypeScript</p>
+        <Image src="./framer.svg" width={50} height={50} className="mt-8 ml-[80px]"></Image>
+        <p className="mt-[50px] ml-8 font-revalia ">Framer</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
