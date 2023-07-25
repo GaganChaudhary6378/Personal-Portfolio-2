26994e19fa7cc21a431a08ef0a6a09fba628161c
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-07-25 23:51:20 +0530

    .

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -94,7 +94,7 @@ export default function about(props) {
               Contrary to popular belief, Lorem Ipsum is not simply random text.
               It has roots in a piece of classical Latin literature from 45 BC,
               making it over 2000 years old. Richard McClintock, a Latin
-              professor at the Hampden-Sydney College in Virginia, looked up one of
+              professor at Hampden-Sydney College in Virginia, looked up one of
               the more obscure Latin words, consectetur, from a Lorem Ipsum
               passage,
             </p>
